e2756ac2b3f7702acaae62f4235db29063869855
AfanasevStas <afanasevstas96@mail.ru>
2026-09-01 15:27:34 +0300

    HW_1 DONE

---
+++ Blazor/Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        double d = 0;
+        string result;
+        public void power()
+        {
+            if (a == 1)
+            {
+                result = null;
+                result += '0';
+            }
+            if (a == 2)
+            {
+                result = null;
+                result += "0,1";
+            }
+            if (a == 3)
+            {
+                result = null;
+                result += "0,1,1";
+            }
+            if (a > 3)
+            {
+                result = null;
+                result += "0,1,1";
+                b = 1;
+                c = 1;
+                for (int i = 3; i < a; i++)
+                {
+                    d = b;
+                    b += c;
+                    c = d;
+                    result += $",{Convert.ToString(b)}";
+                }
+            }
+        }
+    }
+}
---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        public void power()
+        {
+            c = Math.Pow(a, b);
+        }
+    }
+}
